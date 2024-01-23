808a1fe148c75fadcee50f051fc145cecf4e98ab
IDidntWannaDoThis <48184683+theexplodingcreeper@users.noreply.github.com>
2024-01-23 13:42:01 +0100

    K:ver7:some movement

--- Lakos adventure/Assets/C# skripts/player scripts/PlayerMovemont.cs
+++ Lakos adventure/Assets/C# skripts/player scripts/PlayerMovemont.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovemont : MonoBehaviour
 {
+    private Vector3 movepoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,22 @@ public class PlayerMovemont : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position == movepoint)
+        {
+                
+                movepoint += new Vector3(0, 3.52f* (int)Input.GetAxis("Vertical"), 0);
+
+            if (transform.position == movepoint)
+            {
+                movepoint += new Vector3(3.52f * (int)Input.GetAxis("Horizontal"), 0, 0);
+            }
+                
+
+
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movepoint, 10 * Time.deltaTime);
+        }
     }
 }
