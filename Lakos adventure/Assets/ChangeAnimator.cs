70e675cc981549dd44ce2eaaa01df84258fb404c
IDidntWannaDoThis <48184683+theexplodingcreeper@users.noreply.github.com>
2024-05-24 12:24:46 +0200

    K:3.3:some  scene fix

--- Lakos adventure/Assets/ChangeAnimator.cs
+++ Lakos adventure/Assets/ChangeAnimator.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class ChangeAnimator : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    [SerializeField] private RuntimeAnimatorController nextscene;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (nextscene)
+        {
+            textinteractor.changeanim(nextscene);
+        }
+        else
+        {
+            textinteractor.changeanim();
+        }
+        
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
